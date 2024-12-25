18870c26079dbd7482dc0a6c293ebc1f012dc0e2
Scott Bragg <scottb@aq1systems.com>
2024-12-26 03:15:06 +1100

    Added another package that the bot can use and wrote a test

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,10 +1,32 @@
 // Initializes and runs the Discord bot
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
+import (
+    "fmt"
+    "log"
+    "github.com/faulteh/nap-and-go/pkg/greetings"
+    "rsc.io/quote"
+)
 
 func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("bot: ")
+    log.SetFlags(0)
+
     fmt.Println(quote.Go())
+
+    // A slice of names.
+    names := []string{"Ardenne", "Sam", "Liz"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+
+    // If an error was returned, print it to the console and exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(messages)
 }
